ff951341e09dab6c5332ef609abf0f2aa7e57274
Jorge-Lopes <jorge.lopes@bytepitch.com>
2025-09-22 12:01:20 +0100

    test: Ed25519 2020 fails to create proof

--- test/vc.test.js
+++ test/vc.test.js
@@ -1,9 +1,8 @@
-import { issueCredential, verifyCredential, createPresentation, signPresentation } from "../src/credentials.js"
+import { issueCredential, verifyCredential, createPresentation, signPresentation } from "../src/verifiableCredential.js"
 import { getCustomDocumentLoader } from "./helpers/documentLoader.js"
-import { createDID } from "./helpers/did.js"
-import { getBaseCredential } from "./helpers/baseCredential.js"
-import { promises as fs } from "fs";
-
+import { createDID2018, createDID2020 } from "./helpers/did.js"
+import { getCredential, getSchema } from "./helpers/credential.js"
+import jsigs from "jsonld-signatures";
 
 describe('verifiable credentials', () => {
 
@@ -12,20 +11,19 @@ describe('verifiable credentials', () => {
     let baseCredential
 
     beforeAll(async () => {
-        customDocumentLoader = await getCustomDocumentLoader()
-        issuer = await createDID()
+        issuer = await createDID2018()
+        const { keyPair, assertionController } = issuer
 
-        const { keyPair, didDocument } = issuer
+        customDocumentLoader = await getCustomDocumentLoader()
         const { remoteDocuments } = customDocumentLoader
 
-        remoteDocuments.set(keyPair.controller, didDocument)
+        remoteDocuments.set(keyPair.controller, assertionController)
         remoteDocuments.set(keyPair.id, keyPair.export({ publicKey: true }))
 
-        const subjectSchemaRaw = await fs.readFile("test/helpers/subjectSchema.json", "utf-8")
-        const subjectSchema = JSON.parse(subjectSchemaRaw);
-        remoteDocuments.set("https://schema.org/", subjectSchema)
+        const { schemaURL, schemaContext } = await getSchema()
+        remoteDocuments.set(schemaURL, schemaContext)
 
-        baseCredential = getBaseCredential(keyPair.controller)
+        baseCredential = getCredential(keyPair.controller)
     })
 
     test('issue and verify credential', async () => {
@@ -58,16 +56,15 @@ describe('verifiable credentials', () => {
         const verifiableCredential = await issueCredential(baseCredential, suite, documentLoader)
         const verifiablePresentation = createPresentation(verifiableCredential, 'test:ebc6f1c2', keyPair.controller)
         const signedPresentation = await signPresentation(verifiablePresentation, suite, '12ec21', documentLoader)
-        
+
         expect(signedPresentation).toBeDefined();
     })
 
-    test('canonize proof', async () => {
+    test('create authentication proof', async () => {
         const { keyPair, suite } = issuer
         const { documentLoader } = customDocumentLoader
 
-        const verifiableCredential = await issueCredential(baseCredential, suite, documentLoader)
-        const presentation = createPresentation(verifiableCredential, 'test:ebc6f1c2', keyPair.controller)
+        const presentation = createPresentation(baseCredential, 'test:ebc6f1c2', keyPair.controller)
 
         const proofSet = []
 
@@ -84,5 +81,55 @@ describe('verifiable credentials', () => {
         });
 
         expect(proof).toBeDefined();
+    })
+})
+
+describe('Ed25519Signature2020', () => {
+
+    let customDocumentLoader;
+    let issuer
+    let baseCredential
+
+    beforeAll(async () => {
+        issuer = await createDID2020()
+        const { keyPair, assertionController } = issuer
+
+        customDocumentLoader = await getCustomDocumentLoader()
+        const { remoteDocuments } = customDocumentLoader
+
+        remoteDocuments.set(keyPair.controller, assertionController)
+        remoteDocuments.set(keyPair.id, keyPair.export({ publicKey: true }))
+
+        const { schemaURL, schemaContext } = await getSchema()
+        remoteDocuments.set(schemaURL, schemaContext)
+
+        baseCredential = getCredential(keyPair.controller)
+    })
+
+    test('failing to create authentication proof', async () => {
+        const { keyPair, suite } = issuer
+        const { documentLoader } = customDocumentLoader
+
+        const presentation = createPresentation(baseCredential, 'test:ebc6f1c2', keyPair.controller)
+
+        const proofSet = []
+
+        const { AuthenticationProofPurpose } = jsigs.purposes;
+        const domain = undefined;
+        const challenge = '12ec21'
+        const purpose = new AuthenticationProofPurpose({
+            domain,
+            challenge
+        });
+
+        expect.assertions(1);
+        try {
+            await suite.createProof({
+                document: presentation, purpose, proofSet, documentLoader
+            });
+        } catch (error) {
+            const eventError = error.details.event.message
+            expect(eventError).toMatch('Dropping property that did not expand into an absolute IRI or keyword.');
+        }
     })
 })
